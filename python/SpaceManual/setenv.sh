f58c06fb1b481864d555ceaba765bbc11a500ffe
L.R. McFarland <lrm@bayarea.net>
2014-05-26 12:50:04 -0700

    more verbose setenv.sh

--- python/SpaceManual/setenv.sh
+++ python/SpaceManual/setenv.sh
@@ -5,15 +5,14 @@
 # TODO: add linux support
 #
 
-if [ -z "$ORBITS_ROOT" ]; then
-    ORBITS_ROOT=../../
-    echo "ORBITS_ROOT not set. Using" $ORBITS_ROOT
-else
-    echo "ORBITS_ROOT is" $ORBITS_ROOT
-fi
+# python path
 
-ORBITS_LIBRARY_PATH=${ORBITS_ROOT}/libSpace
-export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${ORBITS_LIBRARY_PATH}
+if [ -z "$ORBITS_ROOT" ]; then
+    ORBITS_ROOT=../..
+    echo "# ORBITS_ROOT not set. Using" $ORBITS_ROOT
+else
+    echo "# ORBITS_ROOT is" $ORBITS_ROOT
+fi
 
 SPACE_SO=`find . -name space.so`
 
@@ -21,9 +20,18 @@ if [ -z "$SPACE_SO" ]; then
     echo "space.so not found"
     exit 1
 else
-    echo "space.so:" $SPACE_SO # TODO
+    echo "# space.so:" $SPACE_SO # TODO
     export PYTHONPATH=${PYTHONPATH}:$(dirname ${SPACE_SO})
 fi
+
+# env
+
+ORBITS_LIBRARY_PATH=${ORBITS_ROOT}/libSpace
+export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${ORBITS_LIBRARY_PATH}
+
+echo '# DYLD_LIBRARY_PATH' ${DYLD_LIBRARY_PATH}
+echo '# PYTHONPATH' ${PYTHONPATH}
+echo # linefeed
 
 
 # EoF
